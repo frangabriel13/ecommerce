8f62cfd4a48aa7d2e7c9f6cc6b02b105f046beec
franco <mansilla.franco.1@gmail.com>
2023-06-27 02:49:26 -0300

    header creado

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import s from "./Header.module.css"
+import logo from "../../assets/mandev2.png"
+
+function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = (sectionId) => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div className={s.container}>
+        <a href='#'>
+          <img src={logo} className={s.logo} />
+        </a>
+        <div className={s.nav}>
+          <nav>
+            <a href='#'>Inicio</a>
+            <a href='#'>¿Cómo comprar?</a>
+            <a href='#'>Tienda</a>
+            <a href='#'>Categorías</a>
+            <a href='#'>Contacto</a>
+          </nav>
+          <div>
+            <i className={`bi bi-search ${s.icon}`}></i>
+            <i className={`bi bi-person ${s.icon}`}></i>
+            <i className={`bi bi-cart3 ${s.icon}`}></i>
+          </div>
+        </div>
+        <div className={s.burger}>
+          <i className={`bi bi-list`} onClick={toggleMenu}></i>
+        </div>
+    </div>
+  );
+}
+
+
+export default Header;
