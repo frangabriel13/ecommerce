5ed3d3c21828d9e4ef035ae8ea0ba5539861d018
franco <mansilla.franco.1@gmail.com>
2023-06-28 00:32:00 -0300

    info detallada

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import s from "./Home.module.css";
 import Banner from "../banner/Banner";
+import Info from "../info/Info"
 
 function Home() {
   return (
     <div className={s.container}>
       <Banner />
+      <Info />
     </div>
   );
 }
