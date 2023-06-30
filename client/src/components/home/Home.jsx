2e91541b7bac4b7db57a6d44ee25edfc4dcd5785
fabio mansilla <fabio.mansilla07@gmail.com>
2023-06-30 15:41:07 -0300

    work card

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import s from "./Home.module.css";
 import Banner from "../banner/Banner";
-import Info from "../info/Info"
+import Info from "../info/Info";
+import Card from "../card/Card"
 
 function Home() {
   return (
     <div className={s.container}>
       <Banner />
       <Info />
+      <Card />
     </div>
   );
 }
