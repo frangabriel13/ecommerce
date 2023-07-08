c5ad679526280071b223c42de30b9425c2f70510
franco <mansilla.franco.1@gmail.com>
2023-07-08 17:55:38 -0300

    cree componente category

---
+++ client/src/components/dashboard/categoryManagement/CategoryManagement.jsx
@@ -0,0 +1,110 @@
+import React from "react";
+import s from './CategoryManagement.module.css';
+
+function CategoryManagement() {
+  return (
+    <div className={s.container}>
+      <h2>Categorías</h2>
+      <div className={s.categoryContainer}>
+        <div className={s.categories}>
+          <div className={s.search}>
+            <input type="text" placeholder="Buscar..." />
+            <button>Buscar</button>
+            <button>Agregar</button>
+          </div>
+          <div className={s.categoryList}>
+            <div className={s.level1}>
+              {/* Renderizar categorías de nivel 1 */}
+            </div>
+            <div className={s.level2}>
+              {/* Renderizar categorías de nivel 2 */}
+            </div>
+            <div className={s.level3}>
+              {/* Renderizar categorías de nivel 3 */}
+            </div>
+          </div>
+        </div>
+
+        <div className={s.category}>
+          <h3>Pantalones</h3>
+          <div className={s.categoryInfo}>
+            <label>
+              Nombre:
+              <input type="text" />
+            </label>
+            <label>
+              Categoría padre:
+              <select>
+                {/* Renderizar opciones de categorías padre */}
+              </select>
+            </label>
+          </div>
+          <div className={s.subcategories}>
+            <h4>Subcategorías:</h4>
+            {/* Renderizar subcategorías de la categoría seleccionada */}
+          </div>
+          <button>Eliminar categoría</button>
+          <button>Guardar cambios</button>
+        </div>
+
+        <button>Cancelar</button>
+        <button>Guardar cambios</button>
+      </div>
+    </div>
+  );
+}
+
+
+export default CategoryManagement;
+
+
+
+
+// function CategoryManagement() {
+//   return (
+//     <div className={s.container}>
+//       <h2>Categorías</h2>
+//       <div className={s.categoryContainer}>
+//         <div className={s.categoryList}>
+//           <div className={s.searchBar}>
+//             <input type="text" placeholder="Buscar categoría" />
+//             <button>Buscar</button>
+//             <button>Agregar</button>
+//           </div>
+//           <div className={s.categoryLevels}>
+//             <div className={s.categoryLevel1}>
+//               {/* Renderizar categorías de nivel 1 */}
+//             </div>
+//             <div className={s.categoryLevel2}>
+//               {/* Renderizar categorías de nivel 2 */}
+//             </div>
+//             <div className={s.categoryLevel3}>
+//               {/* Renderizar categorías de nivel 3 */}
+//             </div>
+//           </div>
+//         </div>
+//         <div className={s.categoryDetails}>
+//           <h3>Datos de la categoría seleccionada</h3>
+//           <div className={s.categoryInfo}>
+//             <label>
+//               Nombre:
+//               <input type="text" />
+//             </label>
+//             <label>
+//               Categoría padre:
+//               <select>
+//                 {/* Renderizar opciones de categorías padre */}
+//               </select>
+//             </label>
+//           </div>
+//           <div className={s.subcategories}>
+//             <h4>Subcategorías:</h4>
+//             {/* Renderizar subcategorías de la categoría seleccionada */}
+//           </div>
+//           <button>Eliminar</button>
+//           <button>Guardar cambios</button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
---
+++ client/src/components/dashboard/sideBar/SideBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import s from './SideBar.module.css';
+
+function SideBar() {
+  return (
+    <div className={s.sidebar}>
+    </div>
+  );
+}
+
+
+export default SideBar;
