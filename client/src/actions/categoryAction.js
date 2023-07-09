e8e6c6f221ab05100cd603262f14c34ffb7b5598
franco <mansilla.franco.1@gmail.com>
2023-07-09 20:36:17 -0300

    add categoryList

--- client/src/actions/categoryAction.js
+++ client/src/actions/categoryAction.js
@@ -10,3 +10,21 @@ export const getCategories = () => {
     return categories;
   };
 };
+
+// export const postCategory = (payload) => {
+//   return async function(dispatch) {
+//     let response = await instance.post("categories", payload)
+//     return response
+//   }
+// }
+
+export const postCategory = (payload) => {
+  return async function(dispatch) {
+    try {
+      const response = await instance.post("categories", payload);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
+};
